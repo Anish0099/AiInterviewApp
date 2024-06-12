e44fda43ab2e6b0d37f5c0865743a0435272f449
Anish Kumar <kanish92729@gmail.com>
2024-06-12 16:38:05 +0530

    second

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import { Button } from "/components/ui/button";
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
-    <>
-      <h2>Hello</h2>
-      <Button>Click me</Button>
-    </>
+    <div className="flex justify-center items-center gap-5 h-screen w-screen ">
+      <h2 className="font-medium text-primary text-medium">Hello</h2>
+      <Link href="/dashboard">
+        <Button>Go to Dashboard for interview</Button>
+      </Link>
+
+    </div>
   );
 }
